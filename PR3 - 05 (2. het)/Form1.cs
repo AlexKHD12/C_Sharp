cdb1adeb213827d2a9418bbfedb7911945b925eb
AlexKHD12 <100164968+AlexKHD12@users.noreply.github.com>
2023-11-02 13:55:01 +0100

    Add files via upload

---
+++ PR3 - 05 (2. het)/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR3___05__2.het_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_szamitas_Click(object sender, EventArgs e)
+        {
+            double burgonya = 1.20;
+            double repa = 0.99;
+            double kaposzta = 0.75;
+
+            double vegosszeg = Convert.ToDouble(textBox1.Text) * burgonya + Convert.ToDouble(textBox2.Text) * repa + Convert.ToDouble(textBox3.Text) * kaposzta;
+
+            vegosszeg = Math.Round(vegosszeg, 2);
+            label4.Text = "Fizetendő: " + vegosszeg + " EUR";
+        }
+    }
+}
---
+++ PR3___06__3.het/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR3___06__3.het
+{
+    public partial class Form1 : Form
+    {
+        private int szamlalo = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            szamlalo++;
+            if (szamlalo == 1) {
+                button1.Location = new Point(this.ClientSize.Width - button1.Width, 0);
+            }
+            if (szamlalo == 2) {
+                button1.Location = new Point(this.ClientSize.Width - button1.Width, this.ClientSize.Height - button1.Height);
+            }
+            else if (szamlalo == 3)
+            {
+                button1.Location = new Point(0, this.ClientSize.Height - button1.Height);
+            }
+            else {
+                button1.Location = new Point(0, 0);
+                szamlalo = 0;
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (szamlalo == 1)
+            {
+                button1.Location = new Point(this.ClientSize.Width - button1.Width, 0);
+            }
+            if (szamlalo == 2)
+            {
+                button1.Location = new Point(this.ClientSize.Width - button1.Width, this.ClientSize.Height - button1.Height);
+            }
+            else if (szamlalo == 3)
+            {
+                button1.Location = new Point(0, this.ClientSize.Height - button1.Height);
+            }
+        }
+
+
+    }
+}
