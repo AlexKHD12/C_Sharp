dfca0863eb378ff187061bc05735f9f3eb74fc52
AlexKHD12 <100164968+AlexKHD12@users.noreply.github.com>
2023-11-02 13:52:06 +0100

    Add files via upload

---
+++ PR3 - 04 (2. het)/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PR3___04__2.het_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl01_output = new System.Windows.Forms.Label();
+            this.button1_sorsolas = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl01_output
+            // 
+            this.lbl01_output.AutoSize = true;
+            this.lbl01_output.Location = new System.Drawing.Point(12, 9);
+            this.lbl01_output.Name = "lbl01_output";
+            this.lbl01_output.Size = new System.Drawing.Size(0, 13);
+            this.lbl01_output.TabIndex = 0;
+            // 
+            // button1_sorsolas
+            // 
+            this.button1_sorsolas.Location = new System.Drawing.Point(64, 74);
+            this.button1_sorsolas.Name = "button1_sorsolas";
+            this.button1_sorsolas.Size = new System.Drawing.Size(75, 23);
+            this.button1_sorsolas.TabIndex = 1;
+            this.button1_sorsolas.Text = "Sorsolás";
+            this.button1_sorsolas.UseVisualStyleBackColor = true;
+            this.button1_sorsolas.Click += new System.EventHandler(this.button1_sorsolas_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 116);
+            this.Controls.Add(this.button1_sorsolas);
+            this.Controls.Add(this.lbl01_output);
+            this.Name = "Form1";
+            this.Text = "Lottószámok";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl01_output;
+        private System.Windows.Forms.Button button1_sorsolas;
+    }
+}
+
---
+++ PR3 - 04 (2. het)/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR3___04__2.het_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_sorsolas_Click(object sender, EventArgs e)
+        {
+            int[] t = new int[5];
+            Random rnd = new Random();
+            int i = 0;
+
+            do
+            {
+                int x = rnd.Next(1, 91);
+
+                if (!t.Contains(x)) {
+                t[i] = x;
+                i++;
+            } while (i < 5);
+
+            Array.Sort(t);
+
+            lbl01_output.Text = "Számok: ";
+
+            foreach (int item in t) {
+                lbl01_output.Text += item + ", ";
+                }
+            }
+        }
+    }
+}
